47bd221e2ac17ffa99cf5efa7641bde9b93f9f48
Gabriel Alves <gabriel0alves.oficial@gmail.com>
2022-07-29 19:22:58 -0300

    Erros corrigidos + Menu

--- functions/cabecalho.h
+++ functions/cabecalho.h
@@ -17,7 +17,7 @@ void TesteFuncoesVetor();
 
 int **CriaMatriz(int n_rows, int n_columns);
 void ImprimeMatriz(int **M, int n_rows, int n_columns);
-void DesalocaMatriz(int **M, int n_rows);
+void DesalocaMatriz(int ***M, int n_rows);
 void TesteFuncoesMatriz();
 
 /**
@@ -26,5 +26,5 @@ void TesteFuncoesMatriz();
 
 int ***CriaMatriz3D(int n_slices, int n_rows, int n_columns);
 void ImprimeMatriz3D(int ***M, int n_slices, int n_rows, int n_columns);
-void Desaloca3D(int ***M, int n_slices, int n_rows);
+void Desaloca3D(int ****M, int n_slices, int n_rows);
 void Teste3D();
--- functions/matriz.c
+++ functions/matriz.c
@@ -12,25 +12,15 @@ int **CriaMatriz(int n_rows, int n_columns){
      /**
      * @brief Ponteiro duplo que aponta para o vetor de ponteiro que são as linhas da matriz
      */
-    int **M = NULL; 
-    int i = 0;
+    int **M = (int**)calloc(n_rows,sizeof(int*)); 
     
-    /**
-     * Aloca cada coluna da matriz, sendo o ponteiro inicial de cada linha o responsavel por percorrer
-     * o vetor.
-     * Uma matriz é um vetor de vetores (Array/Arrays)
-     */
-    M = (int**)calloc(n_rows,sizeof(int*)); 
-    
-    while(i != n_rows){
-        
+    for(int i = 0; i < n_rows; i++){
+
         M[i] = (int*)calloc(n_columns,sizeof(int));
 
-        i++;
-    
     }
 
-    return (int**)M;
+    return M;
 
 }
 
@@ -62,16 +52,20 @@ void ImprimeMatriz(int **M, int n_rows, int n_columns){
  * @param M Endereço da Matriz a ser desalocada (&M)
  * @param n_rows Quantidade de linhas da matriz
  */
-void DesalocaMatriz(int **M, int n_rows){
+void DesalocaMatriz(int ***M, int n_rows){
+
+    int **aux = *M;
 
     for(int i = 0; i < n_rows; i++){
-        free(M[i]);
+        free(aux[i]);
     }
 
-    free(M);
-    M = NULL;
+    free(aux);
+    *M = NULL;
 
     puts("Matriz desalocada!\n\n");
+
+    printf("M é nulo? %d", *M == NULL);
 
 }
 
@@ -98,9 +92,7 @@ void TesteFuncoesMatriz(){
  */
 int ***CriaMatriz3D(int n_slices, int n_rows, int n_columns){
 
-    int ***M = NULL;
-    
-    M = (int***)calloc(n_slices,sizeof(int**));
+    int ***M = (int***)calloc(n_slices,sizeof(int**));
 
     for(int i = 0; i < n_slices; i++){
 
@@ -112,7 +104,7 @@ int ***CriaMatriz3D(int n_slices, int n_rows, int n_columns){
         }
     }
 
-    return (int***)M;
+    return M;
 }
 
 /**
@@ -148,27 +140,31 @@ void ImprimeMatriz3D(int ***M, int n_slices, int n_rows, int n_columns){
 /**
  * @brief Desaloca matriz de 3 dimensões.
  * 
- * @param M Endereco da Matriz 3D a ser desalocada (&M).
+ * @param M Endereço da Matriz 3D a ser desalocada (&M).
  * @param n_slices Quantidade de latias.
  * @param n_rows Quantidade de linhas.
  */
-void Desaloca3D(int ***M, int n_slices, int n_rows){
+void Desaloca3D(int ****M, int n_slices, int n_rows){
+
+    int ***aux = **M;
 
     for(int i = 0; i < n_slices; i++){
 
         for(int j = 0; j < n_rows; j++){
 
-            free(M[i][j]);
+            free(aux[i][j]);
 
         }
 
-        free(M[i]);
+        free(aux[i]);
     }
 
-    free(M);
-    M = NULL;
+    free(aux);
+    *M = NULL;
 
     puts("Matriz 3D desalocada!\n\n");
+
+    printf("M é nulo? %d", *M == NULL);
 
 }
 
